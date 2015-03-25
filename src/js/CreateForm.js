a1beb84552184809aeb78c5da66dcfeed59ea5b4
airidasj <airidas@wixeb.com>
2015-03-25 21:11:58 +0000

    Deploying form changes

--- src/js/CreateForm.js
+++ src/js/CreateForm.js
@@ -24,12 +24,33 @@ var CreateForm = React.createClass({
 			}
 		}
 
+		if(store.get('removeForm') === true){
+			this.state.formCount -= 1;
+			store.set('removeForm', false);
+		}
+
 		return <div>
 			<header></header>
 			<div className="content">
-				<h1>{"Fill in the fields and generate your on MIXER sequence!"}</h1>
+				<h1>{"Fill in the fields and generate your own MIXER sequence!"}</h1>
 				{forms}
 				<button className="btn" onClick={this.addForm}>{'Add an one more Event!'}</button>
+				<div className="miixer-submit">
+					<h1>{'Submit the Form:'}</h1>
+
+					<div className="col-lg-12">
+						<div className="input input-group input-group-lg">
+						  <span className="input-group-addon" id="sizing-addon1"> <span className="glyphicon glyphicon-star" aria-hidden="true"></span> </span>
+						  <input type={"text"} className="form-control"  placeholder={"The Keyword"} aria-describedby="sizing-addon1" />
+						</div>
+
+						<button className="btn" >{'SUBMIT'}</button>
+
+					</div>
+
+
+
+				</div>
 			</div>
 		</div>;
 	},
@@ -38,8 +59,8 @@ var CreateForm = React.createClass({
 		this.setState({
 			formCount: this.state.formCount+1
 		});
-		this.getIcons();
 	},
+
 
 });
 
@@ -64,8 +85,19 @@ var FormFields = React.createClass({
 			self.setState(obj);
 		}
 	},
+	removeForm: function(number){
+		// console.log(this.props.parent);
+		store.set('removeForm', true);
+	},
+
 
 	render: function(){
+
+		if(this.props.number <= 1){
+			var buttonRemove = '';
+		} else {
+			var buttonRemove = <button onClick={this.removeForm.bind(null, this.props.number)} className="form-remove"></button>;
+		}
 
 		var iconList = store.get('iconNames') || [];
 		var icons = iconList.map(function(icona, i){
@@ -73,11 +105,12 @@ var FormFields = React.createClass({
 		});
 
 		return <div className="form">
-			
+
 			<div className="fields">
+			{buttonRemove}
 			<h1>{"Event number "+this.props.number+"!"}</h1>	
 
-				<div className="col-lg-8">
+				<div className="col-lg-12">
 					<div className="input-group input-group-lg">
 					  <span className="input-group-addon" id="sizing-addon3"> {'Icon'} </span>
 
@@ -90,28 +123,28 @@ var FormFields = React.createClass({
 					</div>
 				</div>
 
-				<div className="col-lg-8">
+				<div className="col-lg-12">
 					<div className="input-group input-group-lg">
 					  <span className="input-group-addon" id="sizing-addon1"> <span className="glyphicon glyphicon-star" aria-hidden="true"></span> </span>
 					  <input type={"text"} className="form-control" value={this.state.line1} onChange={this.setProp("line1")} placeholder={"First Line"} aria-describedby="sizing-addon1" />
 					</div>
 				</div>
 
-				<div className="col-lg-8">
+				<div className="col-lg-12">
 					<div className="input-group input-group-lg">
 					  <span className="input-group-addon" id="sizing-addon1"> <span className="glyphicon glyphicon-star" aria-hidden="true"></span> </span>
 					  <input type={"text"} className="form-control" value={this.state.line2} onChange={this.setProp("line2")} placeholder={"Second Line"} aria-describedby="sizing-addon1" />
 					</div>
 				</div>
 
-				<div className="col-lg-8">
+				<div className="col-lg-12">
 					<div className="input-group input-group-lg">
 					  <span className="input-group-addon" id="sizing-addon1">{'Color'}</span>
 					  <input type={"color"} className="form-control" value={this.state.background} onChange={this.setProp("background")} placeholder={"Background Color"} aria-describedby="sizing-addon1" />
 					</div>
 				</div>
 
-				<div className="col-lg-8">
+				<div className="col-lg-12">
 					<div className="input-group input-group-lg">
 					  <span className="input-group-addon" id="sizing-addon1">{'Time (optional)'}</span>
 					  <input type={"datetime-local"} className="form-control" placeholder={"Time to unlock the event"} aria-describedby="sizing-addon1" />
